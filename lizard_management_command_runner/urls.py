a388d28fc63996dba3bebf7346c6ea042f36fb11
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2018-04-26 15:14:17 +0200

    New style URL pattern list

--- lizard_management_command_runner/urls.py
+++ lizard_management_command_runner/urls.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from django.conf.urls import patterns
 from django.conf.urls import url
 from django.contrib import admin
 from lizard_ui.urls import debugmode_urlpatterns
@@ -12,12 +11,11 @@ from lizard_management_command_runner import views
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', views.CommandPageView.as_view(), name='command_page_view'),
     url(r'^command/(?P<command_id>\d+)/$',
         views.run_command, name="command_run_view"),
     url(r'^runs/$',
         views.last_run_of_all_commands, name="latest_runs_view"),
-    )
+    ]
 urlpatterns += debugmode_urlpatterns()
