ee2747ac68c6350dd51e1b71b1ed0f5b6bab38c4
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2018-05-04 10:27:06 +0200

    REconfigure celery

--- lizard_management_command_runner/tasks.py
+++ lizard_management_command_runner/tasks.py
@@ -1,12 +1,13 @@
-from celery.task import task
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
 
 
-@task
+@shared_task
 def add(x, y):
     """Add two numbers, useful in testing."""
     return x + y
 
 
-@task
+@shared_task
 def run_management_command(user, management_command):
     management_command.run(user)
